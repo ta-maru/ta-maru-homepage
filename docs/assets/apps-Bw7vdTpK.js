7a73de0b8c0e806171dc64ff90b3e01501a59113
Tamaru fromMac <nakanotatsuki132various@gmail.com>
2025-11-20 12:11:47 +0000

    ビルド結果削除

--- docs/assets/apps-Bw7vdTpK.js
+++
@@ -1,8 +0,0 @@
-import{p as s,v as i,w as o}from"./chunk-UIGDSWPH-CWaCCVYV.js";import{P as n}from"./paragraph-DRn5zvH8.js";function l({to:t,imgSrc:a,appName:r,description:e}){return s.jsx(s.Fragment,{children:s.jsx(i,{to:t,className:`block bg-[#D34D00]
-            min-w-48 max-w-72 min-h-12 rounded-xl 
-            group`,children:s.jsxs("div",{className:`bg-white
-                w-full h-full rounded-xl 
-                pl-4 pr-4 pt-4 pb-4
-                sm:group-hover:-translate-4 transition-transform duration-200 stalker-target
-                shadow-xl`,children:[s.jsx("div",{className:`bg-gray-300 w-full h-48 
-                    p-4 mb-4`,children:s.jsx("img",{src:a,className:"w-full h-full object-contain"})}),s.jsx(n,{text:r,className:"text-center font-bold"}),s.jsx(n,{text:e})]})})})}const m="/assets/waqu-logo-B33-4KkY.svg";function c(){return s.jsx("main",{className:"m-auto mt-8 mb-4 pl-8 justify-items-center pr-8",children:s.jsx("div",{className:"grid grid-cols-1 sm:grid-cols-2 gap-8",children:s.jsx(l,{to:"https://waqu.ta-maru.net/",imgSrc:m,appName:"Waqu",description:"簡単に画像に枠をつけることができるアプリ。チェキ風・シネマ風など、テンプレートから選択可能。"})})})}function d({}){return[{title:"アプリ一覧"},{name:"description",content:"た。の作ったアプリ"}]}const x=o(function(){return s.jsx(s.Fragment,{children:s.jsx(c,{})})});export{x as default,d as meta};
